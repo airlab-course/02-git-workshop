39f07064d2177074c91204396bd2ddf7568f5514
ezsilmar <ezsilmar@gmail.com>
2018-10-15 17:13:09 +0500

    starting commit

--- musicbox/musicbox.ino
+++ musicbox/musicbox.ino
@@ -3,12 +3,10 @@
 #include "buzzer.h"
 
 #define PIN_BUTTON_MEL_ONE 3
-#define PIN_BUTTON_MEL_TWO 4
 #define PIN_BUTTON_OFF 5
 #define PIN_BUZZER 6
 
 Button buttonMelodyOne(PIN_BUTTON_MEL_ONE);
-Button buttonMelodyTwo(PIN_BUTTON_MEL_TWO);
 Button buttonOff(PIN_BUTTON_OFF);
 Buzzer buzzer(PIN_BUZZER);
 
@@ -16,9 +14,14 @@ int notes[] = {NOTE_A4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE};
 double durations[] = {8, 1, 4, 1};
 int melodyLength = 4;
 
-int notes2[] = {NOTE_C4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE};
-double durations2[] = {4, 1, 4, 1};
-int melodyLength2 = 4;
+// maybe somewhere in the future we will have one more button...
+// #define PIN_BUTTON_MEL_TWO 4
+// Button buttonMelodyTwo(PIN_BUTTON_MEL_TWO);
+
+// and the second melody
+// int notes2[] = {NOTE_C4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE};
+// double durations2[] = {4, 1, 4, 1};
+// int melodyLength2 = 4;
 
 void setup()
 {
@@ -37,12 +40,6 @@ void loop()
     if (buttonMelodyOne.wasPressed())
     {
         buzzer.setMelody(notes, durations, melodyLength);
-        buzzer.turnSoundOn();
-    }
-
-    if (buttonMelodyTwo.wasPressed())
-    {
-        buzzer.setMelody(notes2, durations2, melodyLength2);
         buzzer.turnSoundOn();
     }
 }
