316901876077063b66a7731c3b386d82b9008b7a
ezsilmar <ezsilmar@gmail.com>
2018-10-15 17:29:58 +0500

    new button for speed

--- musicbox/musicbox.ino
+++ musicbox/musicbox.ino
@@ -4,15 +4,21 @@
 
 #define PIN_BUTTON_MEL_ONE 3
 #define PIN_BUTTON_OFF 5
+#define PIN_BUTTON_SPEED 4
 #define PIN_BUZZER 6
 
 Button buttonMelodyOne(PIN_BUTTON_MEL_ONE);
 Button buttonOff(PIN_BUTTON_OFF);
+Button buttonSpeed(PIN_BUTTON_SPEED);
 Buzzer buzzer(PIN_BUZZER);
 
 int notes[] = {NOTE_A4, NOTE_SILENCE, NOTE_G4, NOTE_SILENCE};
 double durations[] = {8, 1, 4, 1};
 int melodyLength = 4;
+
+unsigned long speeds[] = {25, 50, 100, 200, 400, 800};
+int currentSpeed = 2;
+int speedsLength = 6;
 
 // maybe somewhere in the future we will have one more button...
 // #define PIN_BUTTON_MEL_TWO 4
@@ -41,5 +47,11 @@ void loop()
     {
         buzzer.setMelody(notes, durations, melodyLength);
         buzzer.turnSoundOn();
+    }
+
+    if (buttonSpeed.wasPressed())
+    {
+        currentSpeed = (currentSpeed + 1)%speedsLength;
+        buzzer.setNoteDuration(speeds[currentSpeed]);
     }
 }
