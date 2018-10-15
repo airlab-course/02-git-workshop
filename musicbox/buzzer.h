15b591d6a806dc4c7ce9555015cf6d8adb63dac9
ezsilmar <ezsilmar@gmail.com>
2018-10-15 17:22:43 +0500

    dynamic note duration

--- musicbox/buzzer.h
+++ musicbox/buzzer.h
@@ -1,6 +1,6 @@
 #pragma once
 
-#define BUZZER_NOTE_DURATION 100
+#define BUZZER_DEFAULT_NOTE_DURATION 100
 
 class Buzzer
 {
@@ -17,6 +17,8 @@ public:
         notes = 0;
         durations = 0;
         melodyLength = 0;
+
+        noteDurationMs = BUZZER_DEFAULT_NOTE_DURATION;
     }
 
     void turnSoundOn()
@@ -41,12 +43,17 @@ public:
         melodyLength = _melodyLength;
     }
 
+    void setNoteDuration(unsigned long _noteDurationMs)
+    {
+        noteDurationMs = _noteDurationMs;
+    }
+
     void playSound()
     {
         if (!isEnabled)
             return;
 
-        unsigned long duration = round(BUZZER_NOTE_DURATION*durations[currentNote]);
+        unsigned long duration = round(noteDurationMs*durations[currentNote]);
         if ((millis() - noteStartedMs) > duration)
         {
             int note = notes[currentNote];
@@ -71,4 +78,6 @@ private:
     int* notes;
     double* durations;
     int melodyLength;
+
+    unsigned long noteDurationMs;
 };
